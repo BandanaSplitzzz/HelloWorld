ccd5291c4217c51671f29429e9de0c267fd40c5f
Mason Naphtali <120mnaphtali@frhsd.com>
2019-06-03 10:34:41 -0400

    Actually added hello world, dude

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,8 +1,9 @@
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		System.out.println("yo, whatup world. Im an ai, im gonna take over the world, nerd");
 
 	}
 
